324ac243aff1061deab2cac3def5db3bc925d366
Andre lops <mlops78@hotmail.com>
2017-02-01 23:44:33 -0200

    Inicial Commit

---
+++ app/js/main.js
@@ -0,0 +1,4 @@
+function add(num1, num2) {
+	// deliberately failing the test
+	return num1 * num2
+}
---
+++ test/test.js
@@ -0,0 +1,8 @@
+describe('Add', function() {
+	it('should add two numbers together', function() {
+		// 1 + 2 should = 3
+		expect(add(1, 2)).toBe(3);
+		// 3 + 6 should = 9
+		expect(add(3, 6)).toBe(9);
+	})
+});
