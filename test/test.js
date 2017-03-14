b509cc383079ee5c2788036ace1b12f3f2bf8be8
Andre lops <mlops78@hotmail.com>
2017-03-13 22:09:49 -0300

    Get Travis to install and download bower deps

--- test/test.js
+++ test/test.js
@@ -5,3 +5,11 @@ describe('AddTEste', function() {
 		expect(add(3, 6)).toBe(18);
 	})
 });
+
+
+describe('This test', function() {
+	it('should always return true', function() {
+		// This would fail because true !== false
+		expect(true).toBe(false);
+	});
+})
