e0d08e1582d1cc2cbbfd281b8da4216be41074f8
Andre lops <mlops78@hotmail.com>
2017-03-14 02:03:59 -0300

    detect env with Travis and throw errors

--- gulpfile.js
+++ gulpfile.js
@@ -244,13 +244,18 @@ gulp.task('test', function(done) {
 // =================
 // creating a new task that is exactly the same as
 // default , except that it doesn’t contain the browserSync or watch tasks.
-gulp.task('dev-ci', function(callback) {
-  runSequence(
-    'clean:dev', ['sprites', 'lint:js', 'lint:scss'], ['sass', 'nunjucks'],
-    callback
-  );
-})
-
+// gulp.task('dev-ci', function(callback) {
+//   runSequence(
+//     'clean:dev', ['sprites', 'lint:js', 'lint:scss'], ['sass', 'nunjucks'],
+//     callback
+//   );
+// })
+	gulp.task('dev-ci', function(callback) {
+		runSequence(
+			'clean:dev', ['sprites', 'lint:js', 'lint:scss'], ['sass', 'nunjucks'],
+			callback
+		);
+	})
 
 // ==================
 // OPTIMIZATION PHASE
